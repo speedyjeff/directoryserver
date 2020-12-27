8d302216514b8c833d7cc3480adb33d047567750
speedyjeff <speedyjeff@github.com>
2020-12-27 07:22:48 -0800

    more aggressively link (to reduce publish size).  Change error message to not leak internal details

--- directoryserver/Program.cs
+++ directoryserver/Program.cs
@@ -46,64 +46,71 @@ namespace directoryserver
         {
             while (http.IsListening)
             {
-                // block to get request
-                var context = await http.GetContextAsync();
-                var contenttype = context.Request.AcceptTypes != null && context.Request.AcceptTypes.Length > 0 ? context.Request.AcceptTypes[0] : "";
-
-                // log the incoming request
-                Console.WriteLine($"{System.Threading.Thread.CurrentThread.ManagedThreadId} {DateTime.Now:o} \"{context.Request.HttpMethod} {contenttype} {context.Request.RawUrl} {options.Protocol}/{context.Request.ProtocolVersion}\" {context.Response.StatusCode}");
-
-                // read file from disk
-                byte[] buffer;
-                var path = "";
-                var responseString = "";
-                var filename = context.Request.RawUrl;
-
-                // default file
-                if (string.Equals(context.Request.RawUrl, "/"))
+                try
                 {
-                    foreach (var file in new string[] { "index.html", "index.htm", "default.html", "default.htm" })
+                    // block to get request
+                    var context = await http.GetContextAsync();
+                    var contenttype = context.Request.AcceptTypes != null && context.Request.AcceptTypes.Length > 0 ? context.Request.AcceptTypes[0] : "";
+
+                    // log the incoming request
+                    Console.WriteLine($"{System.Threading.Thread.CurrentThread.ManagedThreadId} {DateTime.Now:o} \"{context.Request.HttpMethod} {contenttype} {context.Request.RawUrl} {options.Protocol}/{context.Request.ProtocolVersion}\" {context.Response.StatusCode}");
+
+                    // read file from disk
+                    byte[] buffer;
+                    var path = "";
+                    var responseString = "";
+                    var filename = context.Request.RawUrl;
+
+                    // default file
+                    if (string.Equals(context.Request.RawUrl, "/"))
                     {
-                        path = Path.Combine(options.Directory, file);
-                        if (File.Exists(path))
+                        foreach (var file in new string[] { "index.html", "index.htm", "default.html", "default.htm" })
                         {
-                            filename = file;
-                            break;
+                            path = Path.Combine(options.Directory, file);
+                            if (File.Exists(path))
+                            {
+                                filename = file;
+                                break;
+                            }
                         }
                     }
-                }
 
-                // remove leading '/'
-                if (filename.Length > 1 && filename[0] == '/') filename = filename.Substring(1);
+                    // remove leading '/'
+                    if (filename.Length > 1 && filename[0] == '/') filename = filename.Substring(1);
 
-                // create path
-                path = Path.Combine(options.Directory, filename);
+                    // create path
+                    path = Path.Combine(options.Directory, filename);
 
-                // check if the path is absolute or trying to escape out of the original directory
-                if (!path.StartsWith(options.Directory)) path = "";
+                    // check if the path is absolute or trying to escape out of the original directory
+                    if (!path.StartsWith(options.Directory)) path = "";
 
-                // read from disk
-                if (File.Exists(path))
-                {
-                    using (var stream = File.OpenRead(path))
+                    // read from disk
+                    if (File.Exists(path))
                     {
-                        buffer = new byte[stream.Length];
-                        await stream.ReadAsync(buffer);
+                        using (var stream = File.OpenRead(path))
+                        {
+                            buffer = new byte[stream.Length];
+                            await stream.ReadAsync(buffer);
+                        }
+                    }
+                    else
+                    {
+                        responseString = $"<HTML><BODY>File not found {filename}</BODY></HTML>";
+                        buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                        contenttype = "text/html";
+                    }
+
+                    // write
+                    context.Response.ContentLength64 = buffer.Length;
+                    context.Response.ContentType = contenttype;
+                    using (var output = context.Response.OutputStream)
+                    {
+                        await output.WriteAsync(buffer, 0, buffer.Length);
                     }
                 }
-                else
+                catch (Exception e)
                 {
-                    responseString = $"<HTML><BODY>File not found {path}</BODY></HTML>";
-                    buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-                    contenttype = "text/html";
-                }
-
-                // write
-                context.Response.ContentLength64 = buffer.Length;
-                context.Response.ContentType = contenttype;
-                using (var output = context.Response.OutputStream)
-                {
-                    await output.WriteAsync(buffer, 0, buffer.Length);
+                    Console.WriteLine($"exception : {e}");
                 }
             }
         }
